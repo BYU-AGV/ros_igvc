1e5b9cf2b06630de19cb43c8ca35d994c0e20f92
idraper <isaac@irslm.org>
2019-01-24 09:16:34 -0800

    created branch for ros api

--- igvc_ws/src/ros_api/main.py
+++ igvc_ws/src/ros_api/main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
+'''
+This is the core ros module that external users will use to call ROS functions
+Last Updated: 24 Jan 2019
+Author: Isaac
+'''
+
 import rospy
 
 class ROS_Handler(object):
     def __init__(self):
-        print self
+        pass
 
