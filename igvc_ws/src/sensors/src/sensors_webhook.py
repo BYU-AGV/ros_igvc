de3f921ccccd2ca667752fa520e0d124f907c77a
JustBrenkman <JustBrenkman@gmail.com>
2019-01-26 13:24:02 -0800

    Added ros node to the server script

--- igvc_ws/src/sensors/src/sensors_webhook.py
+++ igvc_ws/src/sensors/src/sensors_webhook.py
@@ -14,8 +14,17 @@ def webhook():
 
 @app.route('/ping')
 def ping():
+    rospy.loginfo('/ping')
     return jsonify({'status': 'success'})
 
 
+def startROS():
+    pub = rospy.Publisher('sensor_webhook', coord, queue_size=10)
+    rospy.init_node('sesnor_webhook_server', anonymous=True)
+    rospy.loginfo("Webhook is starting")
+
+
+
 if __name__ == '__main__':
+    startROS()
     app.run(host='192.168.226.132')
