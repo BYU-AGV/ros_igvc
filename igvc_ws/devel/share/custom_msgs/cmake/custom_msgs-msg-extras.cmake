b65eb7efbfeef2e1d2e083add1df008b4c90ffcd
idraper <isaac@zfamily.org>
2019-01-26 19:20:22 -0800

    removed cache for devel and install folders

--- igvc_ws/devel/share/custom_msgs/cmake/custom_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(custom_msgs_MESSAGE_FILES "/home/isaac/Desktop/IGVC_workspace/ros_initial/igvc_ws/src/custom_msgs/msg/coord.msg")
-set(custom_msgs_SERVICE_FILES "")
--- igvc_ws/devel/share/custom_msgs/cmake/custom_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(custom_msgs_MSG_INCLUDE_DIRS "/home/isaac/Desktop/IGVC_workspace/ros_initial/igvc_ws/src/custom_msgs/msg")
-set(custom_msgs_MSG_DEPENDENCIES std_msgs;custom_msgs)
