a3de03044f682e00ad62f5b9eb808530e998f5cf
idraper <isaac@zfamily.org>
2019-01-26 22:04:36 -0800

    Added util functions for converting between dictionaries and messages. Also put this in the examples

--- igvc_ws/src/ros_api/util.py
+++ igvc_ws/src/ros_api/util.py
@@ -2,18 +2,22 @@
 
 '''
 This is a script for basic useful utilities and functions.
-Last edited: 25 Jan 2019
+Last edited: 26 Jan 2019
 Author: Isaac
 '''
 
 import sys
+import json
 
 import rospy
+import std_msgs.msg as std_msgs
+import custom_msgs.msg as msgs
 
-def println (*msg, **kwargs):
+
+def println (*args, **kwargs):
     ''' This is a function that mimicks python3's print
 
-        Args:   *msg - an object to print
+        Args:   *args - an object to print
 
         Kwargs: end - what to print at the end of the string (default '\n')
 
@@ -21,7 +25,7 @@ def println (*msg, **kwargs):
     '''
 
     if 'end' not in kwargs: kwargs['end'] = '\n'
-    sys.stdout.write(''.join(map(str, msg)).strip() + kwargs['end'])
+    sys.stdout.write(''.join(map(str, args)).strip() + kwargs['end'])
     sys.stdout.flush()
 
 def ros_spin():
@@ -63,4 +67,51 @@ def ros_sleep(time):
     '''
 
     rospy.sleep(time)
+
+
+def json_to_msg(j_str, msg_type):
+    ''' Converts a JSON string to a message object
+
+        Args:   j_str - a JSON formatted string
+                msg_type - a message class to convert to
+
+        Returns:    A message object of msg_type
+    '''
+
+    return msg_type(**(json.loads(j_str)))
+
+def dict_to_msg(d, msg_type):
+    ''' Converts a dictionary to a message object
+
+        Args:   d - a dictionary (no more than depth 1)
+                msg_type - a message class to convert to
+
+        Returns:    A message object of msg_type
+    '''
+
+    return msg_type(**d)
+
+def msg_to_dict(m):
+    ''' Converts a message object to a dictionary
+        
+        Args:   m - a message object
+
+        Returns:    A dictionary of the format:
+                        key - the variable name
+                        val - the variables value
+    '''
+
+    return {e : getattr(m,e) for e in m.__slots__}
+
+def msg_to_json(m):
+    ''' Converts a message object to a JSON formatted string
+
+        Args:   m - a message object
+
+        Returns:    A JSON formatted string of the format:
+                        key - the variable name
+                        val - the variables value
+    '''
+
+    return json.dumps(msg_to_dict(m))
 
