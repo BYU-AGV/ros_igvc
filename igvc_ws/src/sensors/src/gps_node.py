611d6ed5c8e69d44c8e4be00d0823ab52af61371
JustBrenkman <JustBrenkman@gmail.com>
2019-01-30 10:50:42 -0800

    Added service file for waypoint calculations, distance and orientation

--- igvc_ws/src/sensors/src/gps_node.py
+++ igvc_ws/src/sensors/src/gps_node.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python
 
+'''
+    Discription: GPS ROS node, keeps track of the last known GPS location
+'''
+
 import ros_api as ros
 from ros_api import println
 import custom_msgs.msg as msgs
+import rospy
+
+last_gps_data = None
 
 
 '''
     This is the callback for the subscriber
 
-    args: data - this is the message object from the even
+    args: gps_data - this is the message object from the even
                  is the same type that the subscriber was set up to recieve
 '''
-def callback(data):
-    println("Data: {} ".format(data))
+def callback(gps_data):
+    global last_gps_data
+    last_gps_data = gps_data
+    println("Data: {} ".format(gps_data))
 
 
 '''
-This starts up the node
+    This starts up the node, both the subscriber and service
 '''
 def start_listening():
     sub = ros.ROS_Subscriber('gps_node', 'gps_location', msgs.gps, callback)
