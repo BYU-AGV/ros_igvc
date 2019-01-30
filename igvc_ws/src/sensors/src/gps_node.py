ea83d5c4e91f09170055e2d9de757eb8c57c34d9
JustBrenkman <JustBrenkman@gmail.com>
2019-01-30 10:18:01 -0800

    Updated the gps node

--- igvc_ws/src/sensors/src/gps_node.py
+++ igvc_ws/src/sensors/src/gps_node.py
@@ -2,17 +2,25 @@
 
 import ros_api as ros
 from ros_api import println
-import rospy
 import custom_msgs.msg as msgs
 
 
+'''
+    This is the callback for the subscriber
+
+    args: data - this is the message object from the even
+                 is the same type that the subscriber was set up to recieve
+'''
 def callback(data):
     println("Data: {} ".format(data))
 
 
+'''
+This starts up the node
+'''
 def start_listening():
     sub = ros.ROS_Subscriber('gps_node', 'gps_location', msgs.gps, callback)
-    rospy.spin()
+    ros.ros_spin()
 
 
 
