c54e7fbf8ec76689db569223ac825f3353c0283e
idraper <isaac@zfamily.org>
2019-02-07 18:03:17 -0800

    added a visualize script

---
+++ igvc_ws/src/path_planning/src/visualize.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+'''
+Description: This is a script for visualizing the global map
+Last Modified: 7 Feb 2019
+Author: Isaac Draper
+'''
+
+import std_msgs.msg as std_msgs
+import custom_msgs.msg as msgs
+
+import ros_api as ros
+from ros_api import println
+
+def dispay_graph(g_map):
+    pass
+
+if __name__ == '__main__':
+    pass
+
