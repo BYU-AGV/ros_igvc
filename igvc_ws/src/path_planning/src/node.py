7140fc307b95de3ad2f745f36dc25c5dafce4830
idraper <isaac@zfamily.org>
2019-02-07 21:51:24 -0800

    added comments for node.py

--- igvc_ws/src/path_planning/src/node.py
+++ igvc_ws/src/path_planning/src/node.py
@@ -13,9 +13,21 @@ import ros_api as ros
 from ros_api import println
 
 class Node(object):
-    num = 0
+    ''' A representation of a location in 2-d space, with a range '''
+
+    num = 0     # to keep track of how many nodes there are
     
     def __init__(self, latitude, longitude, radius, parent):
+        ''' Constructor for a Node object
+
+            Args:   latitude - a gps latitude location
+                    longitude - a gps longitude location
+                    radius - the accuracy of the gps reading
+                    parent - the previous Node traveled from
+
+            Returns:    A Node object
+        '''
+
         self.lat = latitude
         self.lon = longitude
         self.r = radius
@@ -32,18 +44,36 @@ class Node(object):
         return 'Node: ({},{}) Parents: {}'.format(self.lat, self.lon, len(self.parents))
 
     def add_parent(self, parent):
+        ''' Adds a Node as a parent if not already added
+
+            Args:   parent - the Node to add
+
+            Returns:    True if added, False if not
+        '''
+
         if parent not in self.parents:
             self.parents.append(parent)
+            return True
+        else:
+            return False
 
     def get_latitude(self):
+        ''' Returns the latitude of this node '''
+
         return self.lat
 
     def get_longitude(self):
+        ''' Returns the longitude of this node '''
+
         return self.lon
 
     def get_parents(self):
+        ''' Returns the parents of this node '''
+
         return self.parents
 
     def get_radius(self):
+        ''' Returns the radius (accuracy) of this node '''
+
         return self.r
 
