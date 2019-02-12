fd70fc15bfb043e7fb460206de2181c04f0752ce
JustBrenkman <JustBrenkman@gmail.com>
2019-02-12 10:55:50 -0800

    Added functions to gps_node that will calculate vector, heading and distance between two waypoints

--- igvc_ws/src/sensors/src/gps_node.py
+++ igvc_ws/src/sensors/src/gps_node.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
-
 '''
     Discription: GPS ROS node, keeps track of the last known GPS location
+    Date Modified: 12 Feb 2019
+    Author: Ben Brenkman
 '''
 
 import ros_api as ros
 from ros_api import println
 import custom_msgs.msg as msgs
 import custom_msgs.srv as srv
-import rospy
+import rospy # TODO once ros_api can handle services switch to use ros_api
 import math
 
-last_gps_data = None
+last_gps_data = None # last gps location, TODO replace when we have a gps sensor, put into an object
 
 
 '''
@@ -20,8 +21,36 @@ last_gps_data = None
     agrs: requet - it is the request .srv message
 '''
 def location_to_waypoint(request):
-    dist = waypoint_to_waypoint(last_gps_data, request)
-    return srv.location_to_waypointResponse(0, 0, 0, dist, 0)
+    dist = waypoint_to_waypoint_dist(last_gps_data, request)
+    head = waypoint_to_waypoint_head(last_gps_data, request)
+    x, y, z = dist_head_to_vector(dist, head)
+    return srv.location_to_waypointResponse(x, y, z, dist, head)
+
+
+'''
+    This function will convert distance and heading into a vector, pointing from the robot to the waypoint
+'''
+def dist_head_to_vector(distance, heading):
+    x = math.cos(heading * math.pi / 180.0) * distance
+    y = math.sin(heading * math.pi / 180.0) * distance
+    z = 0 # TODO use the ailtiude to calulate the z
+    return x, y, z
+
+
+'''
+    This function will take two waypoints and calculate the forward azimuth or heading. This is the initial bearing, 
+    it will point in the direction that will take you in a straight line to the target waypoint.
+
+    args: way1 - a gps waypoint of type msgs.gps
+          way2 - the second gps waypoint of the saem type
+'''
+def waypoint_to_waypoint_head(way1, way2):
+    if way1 == None or way2 == None:
+        return 0
+    x = math.sin(way2.longitude - way1.longitude) * math.cos(way2.latitude)
+    y = math.cos(way1.latitude) * math.sin(way2.latitude) - math.sin(way1.latitude) * math.cos(way2.latitude) * math.cos(way2.longitude - way1.longitude)
+    bearing = math.atan2(y, x) * 180.0 / math.pi
+    return bearing
 
 
 '''
@@ -30,7 +59,7 @@ def location_to_waypoint(request):
     args: way1 - a waypoint of the type msgs.gps
           way2 - second waypoint of the same type
 '''
-def waypoint_to_waypoint(way1, way2):
+def waypoint_to_waypoint_dist(way1, way2):
     if way1 == None or way2 == None:
         return 0
     # Do some fancy calculations to figure out stuff
@@ -56,9 +85,11 @@ def create_service_proxy():
 '''
 def callback(gps_data):
     global last_gps_data
-    println("Distance: " + str(location_to_waypoint(gps_data).distance))
+    heading_data = location_to_waypoint(gps_data)
+    println("Change: {} ".format(heading_data))
     last_gps_data = gps_data
     println("Data: {} ".format(gps_data))
+    println("")
 
 
 '''
